5f715c28af0c3d234c0fbcc77d02efd7221660b6
Michał <sukowskimichal@gmail.com>
2021-11-28 12:10:03 +0100

    infinity slider on opinions

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 console.log("Main js");
+
+/* For displaying opinions on page */
+const slide = document.querySelector(".slides");
+let slides = document.querySelectorAll(".opinion");
+let index = 1;
+const interval = 7000;
+
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
+
+firstClone.id = "first-clone";
+lastClone.id = "last-clone";
+
+slide.append(firstClone);
+slide.prepend(lastClone);
+
+const slideWidth = slides[index].clientWidth;
+
+slide.style.transform = `translateX(${-slideWidth * index}px)`;
+
+const startSlide = () => {
+  setInterval(() => {
+    index++;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+    slide.style.transition = "1s";
+  }, interval);
+};
+
+slide.addEventListener("transitionend", () => {
+  let slides = document.querySelectorAll(".opinion");
+  if (slides[index].id === firstClone.id) {
+    slide.style.transition = "none";
+    index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+});
+
+startSlide();
